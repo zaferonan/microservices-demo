ddf4f025e25eba76e492da192864213be34f3e8b
zafero2 <zaferonan_1995@hotmail.com>
2023-06-30 17:00:31 +0300

    Initial commit

---
+++ src/test/java/com/zaferonan/microservicesdemo/MicroservicesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaferonan.microservicesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
