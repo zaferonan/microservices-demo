03ed994186d3fe90853981ec4f7428521b34f3b6
zafero2 <zaferonan_1995@hotmail.com>
2023-07-02 23:27:00 +0300

    Some Security confs and config server containerizated for docker

--- config-server/src/main/java/com/microservices/demo/config/server/ConfigServer.java
+++ config-server/src/main/java/com/microservices/demo/config/server/ConfigServer.java
@@ -1,13 +1,11 @@
 package com.microservices.demo.config.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-@EnableAutoConfiguration
 public class ConfigServer {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServer.class,args);
